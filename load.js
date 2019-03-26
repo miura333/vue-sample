e202018fe865e6cc7ea0b97ba1f131729d992ff3
miura333 <miutex@gmail.com>
2019-03-26 16:37:15 +0900

    first commit

---
+++ load.js
@@ -0,0 +1,8 @@
+Vue.component('loading', {
+    template:   '<div id="loader-bg">' +
+                    '<div id="loader">' +
+                        '<img src="ajax-loader.gif" width="80" height="80" alt="Now Loading..." />' +
+                        '<p>Now Loading...</p>' +
+                    '</div>' +
+                '</div>'
+});
